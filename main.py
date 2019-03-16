5a485a53b79be0c325d17072d5d12367e12c24b2
Michał Wróblewski <4815656-DeBos@users.noreply.gitlab.com>
2019-03-16 12:21:44 +0000

    Update main.py

--- main.py
+++ main.py
@@ -14,9 +14,9 @@ parser=ArgumentParser()
 parser.add_argument('-t','--threads',type=int,help='set number of the threads',default=15)
 parser.add_argument('-u','--url',help='set url of the video',default='',required=True)
 parser.add_argument('-d','--duration',help='set the duration of the video in seconds',default=5*60)
-parser.add_argument('-p','--proxies',help='set the path to the proxies list')
+parser.add_argument('-p','--proxies',help='set the path of the proxies list')
 parser.add_argument('-us','--user-agent',help='set the user agent for the driver')
-parser.add_argument('-uss','--user-agents',help='set the path to the list of the user agents for the driver')
+parser.add_argument('-uss','--user-agents',help='set the path of the list of the user agents for the driver')
 args=parser.parse_args()
 
 def bot(url):
