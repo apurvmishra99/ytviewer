5650c9dc64d59fe6e039a829a64f3514634cc1e7
Michał Wróblewski <4815656-DeBos@users.noreply.gitlab.com>
2019-03-23 12:17:11 +0000

    Optimize support for custom user agent

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import re
 import requests
-from os import _exit
+from os import _exit,path
 from time import sleep
 from random import choice,uniform
 from argparse import ArgumentParser
@@ -16,7 +16,6 @@ parser.add_argument('-u','--url',help='set url of the video',default='',required
 parser.add_argument('-d','--duration',help='set the duration of the video in seconds',type=float)
 parser.add_argument('-p','--proxies',help='set the path of the proxies list')
 parser.add_argument('-us','--user-agent',help='set the user agent for the driver')
-parser.add_argument('-usl','--user-agents',help='set the path of the list of the user agents for the driver')
 parser.add_argument('-dr','--driver',help='set the driver for the bot',choices=['chrome','firefox'],default='chrome')
 args=parser.parse_args()
 
@@ -26,13 +25,14 @@ def bot(url):
 			proxy.http_proxy=choice(proxies)
 			proxy.ssl_proxy=proxy.http_proxy
 			print(proxy.http_proxy)
+			user_agent=choice(user_agents) if args.user_agent else user_agents.random
 			if args.driver=='chrome':
-				chrome_options.add_argument('user-agent="{}"'.format(args.user_agent or (args.user_agents and choice(user_agents)) or agent.random))
+				chrome_options.add_argument('user-agent="{}"'.format(user_agent))
 				capabilities=webdriver.DesiredCapabilities.CHROME
 				proxy.add_to_capabilities(capabilities)
 				driver=webdriver.Chrome(options=chrome_options,desired_capabilities=capabilities)
 			else:
-				firefox_profile.set_preference('general.useragent.override',args.user_agent or (args.user_agents and choice(user_agents)) or agent.random)
+				firefox_profile.set_preference('general.useragent.override',user_agent)
 				firefox_profile.set_preference('network.proxy.type',1)
 				firefox_profile.set_preference('network.proxy.http',proxy.http_proxy.split(':')[0])
 				firefox_profile.set_preference('network.proxy.http_port',proxy.http_proxy.split(':')[1])
@@ -57,10 +57,13 @@ try:
 	print('%d proxies successfully loaded!'%len(proxies))
 	proxy=Proxy()
 	proxy.proxy_type=ProxyType.MANUAL
-	if args.user_agents:
-		user_agents=open(args.user_agents,'r').read().split('\n')
+	if args.user_agent:
+		if path.isfile(args.user_agent):
+			user_agents=list(filter(None,open(args.user_agent,'r').read().split('\n')))
+		else:
+			user_agents=[args.user_agent]
 	else:
-		agent=UserAgent()
+		user_agents=UserAgent()
 	if args.driver=='chrome':
 		chrome_options=webdriver.ChromeOptions()
 		chrome_options.add_argument('--mute-audio')
