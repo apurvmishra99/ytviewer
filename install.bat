a3b2d3898d2b067a268b252acd630bd62be2f580
Michał Wróblewski <4815656-DeBos@users.noreply.gitlab.com>
2019-05-24 17:29:33 +0100

    Add error handling to install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,10 @@
 title YTViewer installer
 color 0a
+where python
+if %errorlevel% neq 0 (
+	echo Python is not installed or it is not in PATH.
+	echo You can install it from https://www.python.org/
+	exit 1
+)
 pip install -r requirements.txt
 echo Successfully installed YTViewer.
